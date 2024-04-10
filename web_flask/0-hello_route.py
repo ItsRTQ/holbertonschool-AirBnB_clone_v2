3d186208602c257ee4fa42cfdfb96d4f9ac2ba75
William <titanbabrisa@gmail.com>
2024-04-09 23:30:03 -0400

    better documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,15 +2,16 @@
 """ This modules creates a super basic flask local server """
 from flask import Flask
 
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.route("/")
 def home_page():
     """This method just returns simple text"""
 
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,9 +2,9 @@
 """This module creates a local server with flask with 2 routes"""
 from flask import Flask
 
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
 
 @app.route("/")
 def home_page():
@@ -12,11 +12,13 @@ def home_page():
 
     return "Hello HBNB!"
 
+
 @app.route("/hbnb")
 def hbnb():
     """This method send the data for the hbnb route"""
 
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
