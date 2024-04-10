8157ddb6de8a9e962241d97a30a5420864e6395e
William <titanbabrisa@gmail.com>
2024-04-10 04:45:12 -0400

    commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import render_template
 from models import storage
+from models.state import State
 from os import getenv
 
 
@@ -14,12 +15,16 @@ app.url_map.strict_slashes = False
 def state_list():
     """This method fetchs data from storage to display the list of states"""
 
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    states = storage.all()
+    sorted_data = sorted(states.values(), key=lambda state: state.name)
+    for state in sorted_data:
+        state.id = str(state.id)
+        state.name = str(state.name)
+    return render_template("7-states_list.html", states=sorted_data)
 
 
 @app.teardown_appcontext
-def close_storage(exception=None):
+def close_storage():
     """This method closes storage session if storage db"""
 
     if getenv('HBNB_TYPE_STORAGE') == "db":
