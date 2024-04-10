15a74a430234e472ca94f1371e270231b1abb51c
William <titanbabrisa@gmail.com>
2024-04-09 23:03:29 -0400

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+from flask import Flask
+""" This modules creates a super basic flask local server """
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route("/")
+def hbnb():
+    """This method just returns simple text"""
+
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
