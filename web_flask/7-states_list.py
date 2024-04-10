4cd604106ecd946435c60e1b13fee37f3cf3c772
William <titanbabrisa@gmail.com>
2024-04-10 04:01:48 -0400

    trying to fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import render_template
 from models import storage
+from models.state import State
 from os import getenv
 
 
@@ -22,7 +23,7 @@ def close_storage():
 def state_list():
     """This method fetchs data from storage to display the list of states"""
 
-    states = storage.all("State")
+    states = storage.all(State)
     sorted_data = sorted(states.values(), key=lambda state: state.name)
     return render_template("7-states_list.html", states=sorted_data)
 
