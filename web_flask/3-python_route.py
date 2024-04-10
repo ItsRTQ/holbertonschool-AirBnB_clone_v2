f998bddbf15d494aba5564ee34885aba2bf13c44
William <titanbabrisa@gmail.com>
2024-04-09 23:53:17 -0400

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,1 +1,44 @@
 #!/usr/bin/python3
+"""This module creates a local server with flask with 2 routes"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home_page():
+    """This method send the data for the homepage"""
+
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """This method send the data for the hbnb route"""
+
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """This method takes data from the url to print text"""
+
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python", defaults={"text": None})
+@app.route("/python/<text>")
+def python_text(text):
+    """This method takes data from the url to print text"""
+
+    if text:
+        text = text.replace("_", " ")
+    else:
+        text = "is cool"
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
