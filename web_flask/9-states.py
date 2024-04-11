50517a4e9eb00a268f5ac1de0b20f84fc96dfde0
William <titanbabrisa@gmail.com>
2024-04-11 06:25:38 -0400

    fix pycodestyle and html wording

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -24,14 +24,13 @@ def states(id):
     if id is None:
         states = storage.all(State)
         sorted_states = sorted(states.values(), key=lambda state: state.name)
-        
         return render_template('9-states.html', states=sorted_states)
     else:
         states = storage.all(State)
-        for key, value in states.items():
+        for key, val in states.items():
             if str(id) in key:
-                sorted_cities = sorted(value.cities, key=lambda city: city.name)
-                return render_template('9-states.html', state=value, cities=sorted_cities)
+                sort = sorted(val.cities, key=lambda city: city.name)
+                return render_template('9-states.html', state=val, cities=sort)
         return render_template('9-states.html', not_found=True)
 
 
