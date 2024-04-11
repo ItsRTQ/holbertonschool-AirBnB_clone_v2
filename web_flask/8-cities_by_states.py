1f023ab235f288a9e7a4a80b84aa29a2b99665cd
William <titanbabrisa@gmail.com>
2024-04-11 02:05:47 -0400

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,11 +8,10 @@ from os import getenv
 
 
 app = Flask(__name__, template_folder="templates")
-app.url_map.strict_slashes = False
 
 
-@app.route("/cities_by_states")
-def cities_by_states():
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_list():
     """This method uses storage to display the list of cities by states"""
 
     states = storage.all(State)
