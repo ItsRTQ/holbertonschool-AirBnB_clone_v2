b3588db24d11c879b2a508c92ffaa70b92a04d85
William <titanbabrisa@gmail.com>
2024-04-14 19:19:42 -0400

    va a explotar obigao

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -23,8 +23,7 @@ def sort_data(to_sort={}):
     sorted_keys = sorted(sorting.keys())
     sorted_dict = {key: sorting[key] for key in sorted_keys}
     return sorted_dict
-    
-        
+
 
 @app.teardown_appcontext
 def teardown_db(exception=None):
@@ -39,9 +38,16 @@ def filters_html():
     """Display flask web application, based on storage fetch data"""
 
     states = storage.all(State)
-    for key, val in states.items():
-        pass
-    return render_template("10-hbnb_filters.html", state=states)
+    amenities = storage.all(Amenity)
+    sort = sort_data(states)
+    sorting = {}
+    ame_sort = []
+    for key, val in sort.items():
+        sorting[key] = sorted(val.cities, key=lambda city: city.name)
+    for value in amenities.values():
+        ame_sort.append(value.name)
+    ame_srtd = sorted(ame_sort)
+    return render_template("10-hbnb_filters.html", state=sorting, ame=ame_srtd)
 
 
 if __name__ == "__main__":
