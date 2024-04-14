3f17baac425e81301965dbe28345df9b400d8d31
William <titanbabrisa@gmail.com>
2024-04-14 17:28:39 -0400

    commit

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -9,6 +9,23 @@ from os import getenv
 app = Flask(__name__)
 
 
+def key_spliter(to_split):
+    parts = to_split.split('.')
+    if len(parts) > 1:
+        return parts[1]
+    else:
+        return to_split
+
+def sort_data(to_sort={}):
+    sorting = {}
+    for key, value in to_sort.items():
+        sorting[key_spliter(key)] = value
+    sorted_keys = sorted(sorting.keys())
+    sorted_dict = {key: sorting[key] for key in sorted_keys}
+    return sorted_dict
+    
+        
+
 @app.teardown_appcontext
 def teardown_db(exception=None):
     """This method will close the SQLAlchemy session on teardown"""
@@ -22,6 +39,8 @@ def filters_html():
     """Display flask web application, based on storage fetch data"""
 
     states = storage.all(State)
+    for key, val in states.items():
+        pass
     return render_template("10-hbnb_filters.html", state=states)
 
 
