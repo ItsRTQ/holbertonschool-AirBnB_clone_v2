8e48b98da012bb6fdf2472cd2f515419c521142d
William <titanbabrisa@gmail.com>
2024-04-14 20:15:44 -0400

    no work._.

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,14 +4,13 @@ from flask import Flask
 from flask import render_template
 from models import storage
 from models.state import State
-from os import getenv
 
 
 app = Flask(__name__, template_folder="templates")
 app.url_map.strict_slashes = False
 
 
-@app.route('/cities_by_states/')
+@app.route('/cities_by_states')
 def cities_by_states():
     """This method uses storage to display the list of cities by states"""
 
@@ -23,8 +22,7 @@ def cities_by_states():
 def close_storage():
     """This method closes storage session if storage db"""
 
-    if getenv('HBNB_TYPE_STORAGE') == "db":
-        storage.close()
+    storage.close()
 
 
 if __name__ == '__main__':
