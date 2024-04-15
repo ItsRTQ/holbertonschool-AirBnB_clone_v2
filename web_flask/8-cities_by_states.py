8caa100e9158dbe9fce1a31f33c7e48ced57395b
William <titanbabrisa@gmail.com>
2024-04-14 20:00:40 -0400

    no work._.

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,7 +11,7 @@ app = Flask(__name__, template_folder="templates")
 app.url_map.strict_slashes = False
 
 
-@app.route("/cities_by_states")
+@app.route('/cities_by_states/')
 def cities_by_states():
     """This method uses storage to display the list of cities by states"""
 
