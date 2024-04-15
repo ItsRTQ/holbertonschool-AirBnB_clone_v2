369bc2d4c32a4ef5f36f392b2241396d88460ca8
William <titanbabrisa@gmail.com>
2024-04-14 20:11:31 -0400

    no work._.

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -27,5 +27,5 @@ def close_storage():
         storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
