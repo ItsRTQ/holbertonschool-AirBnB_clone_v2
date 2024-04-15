069b8d5f2e4d02cf7b6e660dde3439282739af43
William <titanbabrisa@gmail.com>
2024-04-14 20:43:58 -0400

    no work._.

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,53 +1,38 @@
 #!/usr/bin/python3
-"""This module will start a flask web application"""
+"""This module will start a Flask web application"""
+
 from flask import Flask, render_template
 from models import storage
-from models.state import State, City
+from models.state import State
 from models.amenity import Amenity
-from os import getenv
 
 app = Flask(__name__)
 
 
-def key_spliter(to_split):
-    parts = to_split.split('.')
-    if len(parts) > 1:
-        return parts[1]
-    else:
-        return to_split
-
-def sort_data(to_sort={}):
-    sorting = {}
-    for key, value in to_sort.items():
-        sorting[key_spliter(key)] = value
-    sorted_keys = sorted(sorting.keys())
-    sorted_dict = {key: sorting[key] for key in sorted_keys}
-    return sorted_dict
+def sort_data(to_sort):
+    return sorted(to_sort, key=lambda x: x.name)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """This method will close the SQLAlchemy session on teardown"""
-
-    if getenv('HBNB_TYPE_STORAGE') == "db":
-        storage.close()
+    """Close the current SQLAlchemy session."""
+    storage.close()
 
 
 @app.route("/hbnb_filters", strict_slashes=False)
 def filters_html():
-    """Display flask web application, based on storage fetch data"""
-
+    """Display a Flask web application based on storage fetched data."""
     states = storage.all(State)
     amenities = storage.all(Amenity)
-    sort = sort_data(states)
-    sorting = {}
-    ame_sort = []
-    for key, val in sort.items():
-        sorting[key] = sorted(val.cities, key=lambda city: city.name)
-    for value in amenities.values():
-        ame_sort.append(value.name)
-    ame_srtd = sorted(ame_sort)
-    return render_template("10-hbnb_filters.html", state=sorting, ame=ame_srtd)
+
+    sorted_states = sort_data(states.values())
+    sorted_amenities = sort_data(amenities.values())
+
+    return render_template(
+        "10-hbnb_filters.html",
+        states=sorted_states,
+        amenities=sorted_amenities
+    )
 
 
 if __name__ == "__main__":
